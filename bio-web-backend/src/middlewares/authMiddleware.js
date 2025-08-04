5e35e7d49b546e42498919f3dcdcf940098a65e8
DESKTOP-171AVQP\Admin <kienn8438@gmail.com>
2025-08-04 16:46:03 +0700

    add

--- bio-web-backend/src/middlewares/authMiddleware.js
+++ bio-web-backend/src/middlewares/authMiddleware.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 
 const authMiddleware = (req, res, next) => {
   const token = req.header('Authorization')?.split(' ')[1]; // Lấy token từ header
+  console.log(token);
   if (!token) {
     return res.status(401).json({ message: 'Bạn chưa đăng nhập!' });
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.DB_SECRETKEY); // Giải mã token
+    const decoded = jwt.verify(token, process.env.SECRET_KEY); // Giải mã token
     req.user = decoded; // Lưu thông tin user vào request
     next(); // Cho phép request tiếp tục
   } catch (error) {
