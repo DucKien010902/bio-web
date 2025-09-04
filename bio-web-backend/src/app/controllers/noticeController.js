af43ce22435d9db2380ea92cb4ff13c4ed670b18
DESKTOP-171AVQP\Admin <kienn8438@gmail.com>
2025-09-04 17:00:21 +0700

    add

--- bio-web-backend/src/app/controllers/noticeController.js
+++ bio-web-backend/src/app/controllers/noticeController.js
@@ -1,13 +1,31 @@
 // controllers/ApproveController.js
-const mongoose = require('mongoose');
 const Notice = require('../model/noticeDB');
 
 class NoticeController {
-  // Lấy tất cả đề xuất hiện có
+  // Lấy tất cả thông báo
   async getAllNotice(req, res) {
     try {
       const data = await Notice.find({});
       return res.status(200).json(data);
+    } catch (error) {
+      return res.status(500).json({ message: 'Lỗi server' });
+    }
+  }
+
+  // Update isViewed = true theo postid
+  async markAsViewed(req, res) {
+    try {
+      const { postid } = req.body; // nhận từ client
+      const updated = await Notice.findOneAndUpdate(
+        { postid }, // tìm theo postid
+        { isViewed: true }, // cập nhật
+        { new: true } // trả về bản ghi sau khi update
+      );
+
+      if (!updated) {
+        return res.status(404).json({ message: 'Không tìm thấy thông báo' });
+      }
+      return res.status(200).json(updated);
     } catch (error) {
       return res.status(500).json({ message: 'Lỗi server' });
     }
--- bio-web-backend/src/routes/noticeRoute.js
+++ bio-web-backend/src/routes/noticeRoute.js
@@ -2,4 +2,5 @@ const express = require('express');
 const route = express.Router();
 const Notice = require('../app/controllers/noticeController');
 route.get('/getall', Notice.getAllNotice);
+route.post('/mark-viewed', Notice.markAsViewed);
 module.exports = route;
